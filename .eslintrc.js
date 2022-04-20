9eefe7eaa8ed3780dfba0424404d305f3ed72487
ian <im-ian@naver.com>
2022-04-20 12:34:13 +0900

    change lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,19 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['./index.js', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: ["./index.js", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
   rules: {},
--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
   env: {
     browser: true,
     node: true,
     es6: true,
   },
-  extends: ['./rules/import.js']
+  extends: ["./rules/import.js"]
     .map(require.resolve)
     .concat([
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
     ]),
-  plugins: ['react', 'react-hooks'],
+  plugins: ["react", "react-hooks"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/jsx-curly-brace-presence': [
-      'error',
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-curly-brace-presence": [
+      "error",
       {
-        props: 'always',
-        children: 'ignore',
+        props: "always",
+        children: "ignore",
       },
     ],
-    'react/require-default-props': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    'jsx-quotes': ['error', 'prefer-single'],
+    "react/require-default-props": "off",
+    "react/function-component-definition": "on",
+    "@typescript-eslint/no-floating-promises": "off",
+    "jsx-quotes": ["error", "prefer-single"],
   },
-  ignorePatterns: ['*.js'],
+  ignorePatterns: ["*.js"],
 };
