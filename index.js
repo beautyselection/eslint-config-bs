784ea7e6d97812b59d81294ada0752535ba5941a
ian <im-ian@naver.com>
2022-05-06 10:55:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ module.exports = {
       "plugin:@typescript-eslint/recommended",
       "plugin:@typescript-eslint/recommended-requiring-type-checking",
     ]),
-  plugins: ["react-hooks"],
+  plugins: ["react", "react-hooks"],
   rules: {
     "@typescript-eslint/no-floating-promises": "off",
     "@typescript-eslint/no-unused-vars": [
